2d0294bb9de12bd4a3f7be8862813ee9107cb085
up240460-commits <up240460@alumnos.upa.edu.mx>
2026-02-22 15:26:36 -0600

    Evidencia de tarea COMPONENTES.

--- src/Helper.elm
+++ src/Helper.elm
@@ -3,9 +3,33 @@ module Helper exposing (..)
 import Html
 import Html.Attributes
 
+joinWord : String -> String -> String
+joinWord str1 str2 =
+    str1 ++ str2
 
 
--- Puedes usar una definición como la siguiente para probar y visualizar tus resultados, solo debes definir "headers" y "hyperlink"
---main : Html.Html msg
---main =
---    Html.div [] [ headers "Titulos", hyperlink "https://upa.edu.mx" "My School" ]
+isUpperChars : List Char -> List Bool
+isUpperChars chars =
+    List.map Char.isUpper chars
+
+
+evalChar : List Char -> (Char -> Bool) -> List Bool
+evalChar chars predicate =
+    List.map predicate chars
+
+
+headers : String -> Html.Html msg
+headers content =
+    Html.div []
+        [ Html.h1 [] [ Html.text content ]
+        , Html.h2 [] [ Html.text content ]
+        , Html.h3 [] [ Html.text content ]
+        , Html.h4 [] [ Html.text content ]
+        , Html.h5 [] [ Html.text content ]
+        , Html.h6 [] [ Html.text content ]
+        ]
+
+
+hyperlink : String -> String -> Html msg
+hyperlink url label =
+    a [ href url ] [ text label ]
